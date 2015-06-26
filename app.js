6b7d0f5218a431e2aafb7eeb29516401304e8d53
BV Satyaram <bvsatyaram@gmail.com>
2015-06-26 12:12:19 +0530

    All filter buttons toggleable, except the 'all' filter

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $('#portfolio a').click(function(ev) {
     ev.preventDefault();
-    $('#portfolio .project-thumbnail').addClass('fadeOut');
-    $('#portfolio .project-thumbnail' + this.dataset.filter).removeClass('fadeOut');
+    // $('#portfolio .project-thumbnail').addClass('fadeOut');
+    // $('#portfolio .project-thumbnail' + this.dataset.filter).removeClass('fadeOut');
+    $(this).toggleClass('inactive');
+    $('#portfolio .project-thumbnail' + this.dataset.filter).toggleClass('fadeOut');
   });
 });
