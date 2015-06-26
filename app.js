8cf63d84d7c04d8b6b1cfcf9fbcb6d3cc81729d9
BV Satyaram <bvsatyaram@gmail.com>
2015-06-26 12:24:59 +0530

    handlign complications involved with 'all' filter

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
   $('#portfolio a').click(function(ev) {
     ev.preventDefault();
-    // $('#portfolio .project-thumbnail').addClass('fadeOut');
-    // $('#portfolio .project-thumbnail' + this.dataset.filter).removeClass('fadeOut');
-    $(this).toggleClass('inactive');
-    $('#portfolio .project-thumbnail' + this.dataset.filter).toggleClass('fadeOut');
+    if ($(this).hasClass('all')) {
+      if ($(this).hasClass('inactive')) {
+        $('#portfolio a').removeClass('inactive');
+        $('#portfolio .project-thumbnail').removeClass('fadeOut');
+      } else {
+        $('#portfolio a').addClass('inactive');
+        $('#portfolio .project-thumbnail').addClass('fadeOut');
+      }
+    } else {
+      if ($(this).hasClass('inactive')) {
+        if($('#portfolio a.inactive').length == 2) {
+          $('#portfolio a.all').removeClass('inactive');
+        }
+      } else {
+        $('#portfolio a.all').addClass('inactive');
+      }
+      $(this).toggleClass('inactive');
+      $('#portfolio .project-thumbnail' + this.dataset.filter).toggleClass('fadeOut');
+    }
   });
 });
